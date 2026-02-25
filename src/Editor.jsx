f40cbf2182919560db8643af24e6712dea843031
Ngô Minh Hoàng <110410039+nmhoang0511@users.noreply.github.com>
2026-02-25 21:19:01 +0700

    Create Editor.jsx

---
+++ src/Editor.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+
+export default function Editor({ image, onReset }) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.src = image;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+  }, [image]);
+
+  const downloadImage = () => {
+    const link = document.createElement("a");
+    link.download = "photobooth.png";
+    link.href = canvasRef.current.toDataURL("image/png");
+    link.click();
+  };
+
+  return (
+    <div>
+      <canvas ref={canvasRef} style={{ maxWidth: "100%" }} />
+      <br />
+      <button onClick={downloadImage}>Tải ảnh</button>
+      <button onClick={onReset}>Chụp lại</button>
+    </div>
+  );
+}
